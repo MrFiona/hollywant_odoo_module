80a18c97fcfe16ad8e833fe3f291fa93c2f41818
Michael Zhang <1160177283@qq.com>
2016-10-21 08:29:06 -0500

    Add files via upload

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#   2016 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'HollyWant',
+    'category': 'Setting',
+    'summary': 'Property Renting Out Order',
+    'version': '9.0.1.0.0',
+    'website': 'http://www.yourcompany.com',
+    'author': "holly-want",
+    'depends': [
+        'app_api',
+    ],
+    'data': [
+        'views/update_version.xml'
+    ],
+    'images': [],
+    'license': 'AGPL-3',
+    'installable': True,
+}
